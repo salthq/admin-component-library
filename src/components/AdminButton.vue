d95994a2bfe42bad9b9754d5144e4616612946a9
Jason Tame <jason@advantagelearn.com>
2020-05-10 14:11:27 +0200

    Add 'type' prop to admin buttons, bind classes to each of the allowed types, add tests for each

--- src/components/AdminButton.vue
+++ src/components/AdminButton.vue
@@ -2,7 +2,17 @@
   <span class="inline-flex rounded-md shadow-sm">
     <button
       type="submit"
-      class="inline-flex justify-center py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-blue-600 hover:bg-blue-500 focus:ouline-none focus:border-blue-700 focus:shadow-ouline-blue active:bg-blue-700 transiation duration-150 ease-in-out"
+      class="inline-flex justify-center py-2 px-4 border text-sm leading-5 font-medium rounded-md  transition duration-150 ease-in-out"
+      :class="{
+        'text-white border-transparent bg-blue-600 hover:bg-blue-500 focus:ouline-none focus:border-blue-700 focus:shadow-ouline-blue active:bg-blue-700':
+          type == 'primary',
+        'text-white border-transparent bg-blue-400 hover:bg-blue-300 focus:ouline-none focus:border-blue-600 focus:shadow-ouline-blue active:bg-blue-600':
+          type == 'secondary',
+        'text-gray-700 bg-white hover:bg-gray-100 focus:ouline-none focus:border-gray-700 focus:shadow-ouline-gray active:bg-gray-100':
+          type == 'white',
+        'text-white border-transparent bg-red-600 hover:bg-red-500 focus:ouline-none focus:border-red-700 focus:shadow-ouline-red active:bg-red-700':
+          type == 'danger'
+      }"
     >
       {{ title }}
     </button>
@@ -16,6 +26,13 @@ export default {
     title: {
       type: String,
       required: true
+    },
+    type: {
+      default: 'primary',
+      validator: (value) => {
+        return ['danger', 'primary', 'secondary', 'white'].indexOf(value) !== -1
+      },
+      required: false
     }
   }
 }
--- tests/unit/AdminButton.spec.js
+++ tests/unit/AdminButton.spec.js
@@ -1,12 +1,60 @@
-import { shallowMount } from "@vue/test-utils";
-import AdminButton from "@/components/AdminButton.vue";
+import { shallowMount } from '@vue/test-utils'
+import AdminButton from '@/components/AdminButton.vue'
 
-describe("AdminButton.vue", () => {
-  it("renders props.title when passed", () => {
-    const title = "Save";
+let wrapper
+
+const title = 'Save'
+
+beforeEach(() => {
+  wrapper = shallowMount(AdminButton, {
+    propsData: { title }
+  })
+})
+
+afterEach(() => wrapper.destroy)
+
+describe('AdminButton.vue', () => {
+  it('renders props.title when passed', () => {
+    const title = 'New Title'
     const wrapper = shallowMount(AdminButton, {
       propsData: { title }
-    });
-    expect(wrapper.text()).toMatch(title);
-  });
-});
+    })
+    expect(wrapper.text()).toMatch(title)
+  })
+
+  it('has a darker blue background when props.type is primary', () => {
+    const button = wrapper.find('button')
+
+    expect(button.classes('bg-blue-600')).toBe(true)
+  })
+
+  it('has a lighter blue background when props.type is secondary', () => {
+    const wrapper = shallowMount(AdminButton, {
+      propsData: { title, type: 'secondary' }
+    })
+
+    const button = wrapper.find('button')
+
+    expect(button.classes('bg-blue-400')).toBe(true)
+  })
+
+  it('has a white background when props.type is white', () => {
+    const wrapper = shallowMount(AdminButton, {
+      propsData: { title, type: 'white' }
+    })
+
+    const button = wrapper.find('button')
+
+    expect(button.classes('bg-white')).toBe(true)
+  })
+
+  it('has a red background when props.type is danger', () => {
+    const wrapper = shallowMount(AdminButton, {
+      propsData: { title, type: 'danger' }
+    })
+
+    const button = wrapper.find('button')
+
+    expect(button.classes('bg-red-600')).toBe(true)
+  })
+})
