43e9d12a5ab78af7f1d412fc05be1ee0662a5538
Jason Tame <jason@advantagelearn.com>
2020-05-10 14:40:24 +0200

    Add prevent default submit behaviour on form wrapper

--- src/components/AdminFormWrapper.vue
+++ src/components/AdminFormWrapper.vue
@@ -1,15 +1,18 @@
 <template>
-  <div class="bg-white border border-gray-200 rounded shadow-lg max-w-2xl">
+  <form
+    class="bg-white border border-gray-200 rounded shadow-lg max-w-2xl"
+    @submit.prevent="submit"
+  >
     <div class="mx-auto py-12 px-4 sm:px-6 lg:px-8">
       <slot name="form-content"></slot>
     </div>
 
     <div class="px-4 py-3 bg-gray-100 text-right">
       <slot name="form-actions">
-        <admin-button title="Save" />
+        <admin-button @click="$emit('submit')" type="primary" title="Save" />
       </slot>
     </div>
-  </div>
+  </form>
 </template>
 
 <script>
