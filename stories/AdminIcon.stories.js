14da314be6fb79618403bca48b18d33b547c2756
Jason Tame <jason@advantagelearn.com>
2020-05-13 13:39:42 +0200

    Add more icons and stories for each one

--- stories/AdminIcon.stories.js
+++ stories/AdminIcon.stories.js
@@ -7,15 +7,54 @@ export default {
 }
 
 export const iconData = {
-  icon: 'CopyIcon',
   size: 20,
   viewBox: '0 0 12 14'
 }
 
-export const Default = () => ({
+export const Copy = () => ({
   components: { AdminIcon },
   template: `
-    <div class="text-gray-600"><admin-icon :viewBox="iconData.viewBox" :size="iconData.size" :icon="iconData.icon" class="mr-2 text-current fill-current stroke-current" /></div>
+    <div class="text-gray-600"><admin-icon :viewBox="iconData.viewBox" :size="iconData.size" icon="CopyIcon" class="mr-2 text-current fill-current stroke-current" /></div>
+  `,
+  props: {
+    iconData: { default: () => iconData }
+  }
+})
+
+export const Delete = () => ({
+  components: { AdminIcon },
+  template: `
+    <div class="text-gray-600"><admin-icon viewBox="0 0 20 20" :size="iconData.size" icon="DeleteIcon" class="mr-2 text-current fill-current stroke-current" /></div>
+  `,
+  props: {
+    iconData: { default: () => iconData }
+  }
+})
+
+export const Edit = () => ({
+  components: { AdminIcon },
+  template: `
+    <div class="text-gray-600"><admin-icon viewBox="0 0 20 20" :size="iconData.size" icon="EditIcon" class="mr-2 text-current fill-current stroke-current" /></div>
+  `,
+  props: {
+    iconData: { default: () => iconData }
+  }
+})
+
+export const SortAscending = () => ({
+  components: { AdminIcon },
+  template: `
+    <div class="text-gray-600"><admin-icon :viewBox="iconData.viewBox" :size="iconData.size" icon="SortAscIcon" class="mr-2 text-current fill-current stroke-current" /></div>
+  `,
+  props: {
+    iconData: { default: () => iconData }
+  }
+})
+
+export const SortDescending = () => ({
+  components: { AdminIcon },
+  template: `
+    <div class="text-gray-600"><admin-icon :viewBox="iconData.viewBox" :size="iconData.size" icon="SortDescIcon" class="mr-2 text-current fill-current stroke-current" /></div>
   `,
   props: {
     iconData: { default: () => iconData }
